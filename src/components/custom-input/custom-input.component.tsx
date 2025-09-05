05def5c8c558286d35d5c058a720a904224a573a
gt3oliveira <gt3oliveira@gmail.com>
2025-09-04 22:23:23 -0300

    feat: custom-input component

---
+++ src/components/custom-input/custom-input.component.tsx
@@ -0,0 +1,15 @@
+import { FunctionComponent, InputHTMLAttributes } from 'react'
+import { CustomInputContainer } from './custom-input.styles'
+
+interface CustomInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  hasError?: boolean
+}
+
+const CustomInput: FunctionComponent<CustomInputProps> = ({
+  hasError,
+  ...rest
+}) => {
+  return <CustomInputContainer {...rest} hasError={hasError} />
+}
+
+export default CustomInput
