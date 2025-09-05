736d59b020554aa0833a219d3a9b406594238672
gt3oliveira <gt3oliveira@gmail.com>
2025-09-04 21:38:50 -0300

    feat: ensure header component is visible in all pages with to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { FunctionComponent } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/home/home.page'
+import LoginPage from './pages/login/login.page'
+import Header from './components/header/header.component'
 
 const App: FunctionComponent = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   )
