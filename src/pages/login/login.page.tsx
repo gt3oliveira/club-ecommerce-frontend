491e3eafdc5392faf48ef8e3f4f53538bcbc9ec5
gt3oliveira <gt3oliveira@gmail.com>
2025-09-04 22:23:44 -0300

    feat: add login-page component

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -1,3 +1,6 @@
+import { BsGoogle } from 'react-icons/bs'
+import { FiLogIn } from 'react-icons/fi'
+import CustomButton from '../../components/custom-button/custom-button.component'
 import {
   LoginContainer,
   LoginContent,
@@ -5,16 +8,26 @@ import {
   LoginInputContainer,
   LoginSubtitle
 } from './login.styles'
+import CustomInput from '../../components/custom-input/custom-input.component'
 
 const LoginPage = () => {
   return (
     <LoginContainer>
       <LoginContent>
         <LoginHeadline>Entre com a sua conta</LoginHeadline>
-        <button>google</button>
+        <CustomButton type="button" icon={<BsGoogle size={18} />}>
+          Entrar com o Google
+        </CustomButton>
         <LoginSubtitle>ou entre com o seu email</LoginSubtitle>
-        <LoginInputContainer></LoginInputContainer>
-        <LoginInputContainer></LoginInputContainer>
+        <LoginInputContainer>
+          <CustomInput placeholder="Digite seu email" type="email" />
+        </LoginInputContainer>
+        <LoginInputContainer>
+          <CustomInput placeholder="Digite sua senha" type="password" />
+        </LoginInputContainer>
+        <CustomButton type="submit" icon={<FiLogIn size={18} />}>
+          Entrar
+        </CustomButton>
       </LoginContent>
     </LoginContainer>
   )
