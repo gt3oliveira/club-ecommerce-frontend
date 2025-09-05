c501196429c44b566912bc3029a01112223fd55d
gt3oliveira <gt3oliveira@gmail.com>
2025-09-04 21:44:43 -0300

    refactor: remove header component in home-page

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,13 +1,7 @@
 import CategoriesPage from '../../components/categories/categories.component'
-import Header from '../../components/header/header.component'
 
 const HomePage = () => {
-  return (
-    <>
-      <Header />
-      <CategoriesPage />
-    </>
-  )
+  return <CategoriesPage />
 }
 
 export default HomePage
