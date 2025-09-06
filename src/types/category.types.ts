a4af9317ee2b3dd8dcd297c983d8e0289612d5dd
gt3oliveira <gt3oliveira@gmail.com>
2025-09-05 22:17:48 -0300

    feat: add product and category types

--- src/types/category.types.ts
+++ src/types/category.types.ts
@@ -1,39 +1,9 @@
+import { Product } from './product.types'
+
 export interface Category {
   id: string
   name: string
   displayName: string
   imageUrl: string
+  produtcs: Product[]
 }
-
-export const CategoriesMock = [
-  {
-    id: '1',
-    name: 'Category 01',
-    displayName: '1',
-    imageUrl: '1'
-  },
-  {
-    id: '2',
-    name: 'Category 02',
-    displayName: '2',
-    imageUrl: '2'
-  },
-  {
-    id: '3',
-    name: 'Category 03',
-    displayName: '3',
-    imageUrl: '3'
-  },
-  {
-    id: '4',
-    name: 'Category 04',
-    displayName: '4',
-    imageUrl: '4'
-  },
-  {
-    id: '5',
-    name: 'Category 05',
-    displayName: '5',
-    imageUrl: '5'
-  }
-]
