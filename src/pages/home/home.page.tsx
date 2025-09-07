857550ab61f3b2a1f7c1b4084416feab7230c5f9
gt3oliveira <gt3oliveira@gmail.com>
2025-09-07 17:18:34 -0300

    feat: ensure home-page redirects out page if user is not authenticated

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,6 +1,18 @@
+import { useContext, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import CategoriesPage from '../../components/categories/categories.component'
+import { UserContext } from '../../contexts/user.context'
 
 const HomePage = () => {
+  const { isAuthenticated } = useContext(UserContext)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate('/login')
+    }
+  }, [isAuthenticated])
+
   return <CategoriesPage />
 }
 
