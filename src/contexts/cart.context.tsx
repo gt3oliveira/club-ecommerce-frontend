278a1e4298504197ebc2a352b2f18b8367d95244
gt3oliveira <gt3oliveira@gmail.com>
2025-09-08 22:15:03 -0300

    feat: add cart-context

---
+++ src/contexts/cart.context.tsx
@@ -0,0 +1,34 @@
+import { createContext, FunctionComponent, useState } from 'react'
+import { CartProduct } from '../types/cart.types'
+
+interface ICartContext {
+  isVisible: boolean
+  products: CartProduct[]
+  toggleCart: () => void
+}
+
+export const CartContext = createContext<ICartContext>({
+  isVisible: false,
+  products: [],
+  toggleCart: () => {}
+})
+
+const CartContextProvider: FunctionComponent = ({ children }) => {
+  const [isVisible, setIsVisible] = useState(false)
+  const [products] = useState<CartProduct[]>([])
+
+  const toggleCart = () => setIsVisible(!isVisible)
+
+  return (
+    <CartContext.Provider
+      value={{
+        isVisible,
+        products,
+        toggleCart
+      }}>
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+export default CartContextProvider
