2519f3aaf2be253a30a8172de99a02ad4a07da0c
gt3oliveira <gt3oliveira@gmail.com>
2025-09-09 17:32:01 -0300

    feat: add button and handle-add-cart function from incremented product in cart

--- src/components/product-item/product-item.component.tsx
+++ src/components/product-item/product-item.component.tsx
@@ -1,19 +1,32 @@
-import { FunctionComponent } from 'react'
+import { FunctionComponent, useContext } from 'react'
 import {
   ProductContainer,
   ProductImage,
   ProductInfo
 } from './product-item.styles'
 import { Product } from '../../types/product.types'
+import CustomButton from '../custom-button/custom-button.component'
+import { BsCartPlus } from 'react-icons/bs'
+import { CartContext } from '../../contexts/cart.context'
 
 interface ProductItemProps {
   product: Product
 }
 
 const ProductItem: FunctionComponent<ProductItemProps> = ({ product }) => {
+  const { addProductCart } = useContext(CartContext)
+
+  const handleAddToCart = () => {
+    addProductCart(product)
+  }
+
   return (
     <ProductContainer>
-      <ProductImage imageUrl={product.imageUrl} />
+      <ProductImage imageUrl={product.imageUrl}>
+        <CustomButton onClick={handleAddToCart} icon={<BsCartPlus size={18} />}>
+          Adicionar ao Carrinho
+        </CustomButton>
+      </ProductImage>
       <ProductInfo>
         <p>{product.name}</p>
         <p>
