83694ec3b0f7ed4dd1023c3e6556bb3f9acf4439
gt3oliveira <gt3oliveira@gmail.com>
2025-09-10 14:17:32 -0300

    chore: adicionar suporte ao Redux com configuração de store e rootReducer

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { Provider } from 'react-redux'
 import reportWebVitals from './reportWebVitals'
 import { UserContextProvider } from './contexts/user.context'
 import { CategoryContextProvider } from './contexts/category.context'
 import CartContextProvider from './contexts/cart.context'
+import { store } from './store/store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <UserContextProvider>
-      <CategoryContextProvider>
-        <CartContextProvider>
-          <App />
-        </CartContextProvider>
-      </CategoryContextProvider>
-    </UserContextProvider>
+    <Provider store={store}>
+      <UserContextProvider>
+        <CategoryContextProvider>
+          <CartContextProvider>
+            <App />
+          </CartContextProvider>
+        </CategoryContextProvider>
+      </UserContextProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
