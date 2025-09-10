ecf756aabc4d1c0f1896a8821c6e375218b91415
gt3oliveira <gt3oliveira@gmail.com>
2025-09-09 23:31:50 -0300

    feat: adicionar verificação de autenticação na visualização de explore-page

--- src/components/categories-overview/categories-overview.component.tsx
+++ src/components/categories-overview/categories-overview.component.tsx
@@ -3,9 +3,17 @@ import { CategoryContext } from '../../contexts/category.context'
 import LoadingPage from '../loading/loading.component'
 import { Container } from './categories-overview.styles'
 import CategoryOverview from '../category-overview/category-overview.component'
+import { UserContext } from '../../contexts/user.context'
+import { useNavigate } from 'react-router-dom'
 
 const CategoriesOverview: FunctionComponent = () => {
+  const { isAuthenticated } = useContext(UserContext)
+  const navigate = useNavigate()
   const { categories, isLoading, fetchCategories } = useContext(CategoryContext)
+
+  useEffect(() => {
+    if (!isAuthenticated) navigate('/login')
+  }, [])
 
   useEffect(() => {
     fetchCategories()
