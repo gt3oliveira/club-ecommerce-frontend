72e947e8b5253e1a8e5fbc11c039e9bf56e82157
gt3oliveira <gt3oliveira@gmail.com>
2025-09-16 15:09:39 -0300

    test: garante que a quantidade de produtos do carrinho esteja correta

--- src/components/header/header.test.tsx
+++ src/components/header/header.test.tsx
@@ -1,5 +1,6 @@
 import Header from './header.component'
 import { renderWithRedux } from '../../helpers/test.helpers'
+import { CartProduct } from '../../types/cart.types'
 
 describe('Header', () => {
   it('teste de autenticação: botão de signout visivel', () => {
@@ -26,5 +27,37 @@ describe('Header', () => {
 
     getByText(/login/i)
     getByText(/criar conta/i)
+  })
+
+  it('teste de contagem de produtos o carrinho', () => {
+    const products: CartProduct[] = [
+      {
+        id: '1',
+        name: 'Boné',
+        price: 80,
+        quantity: 10,
+        imageUrl: 'image_url'
+      },
+      {
+        id: '2',
+        name: 'Jaqueta',
+        price: 100,
+        quantity: 12,
+        imageUrl: 'image_url'
+      }
+    ]
+
+    const { getByText } = renderWithRedux(<Header />, {
+      preloadedState: {
+        userReducer: {
+          isAuthenticated: true
+        },
+        cartReducer: {
+          products
+        }
+      } as any
+    })
+
+    getByText('22')
   })
 })
